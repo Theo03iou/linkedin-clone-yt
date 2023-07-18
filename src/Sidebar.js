1bb979d6a1d9f8498496cfc46555698d69c7f8fd
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2023-07-18 17:55:50 +0100

    Sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar } from "@mui/material";
+
+function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar_recentItem">
+      <span className="sidebar_hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <img
+          src="https://images.unsplash.com/photo-1528360983277-13d401cdc186?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+          alt=""
+        ></img>
+        <Avatar className="sidebar_avatar" />
+        <h2>Theo Georgiou</h2>
+        <h4>Theo03iou@outlook.com</h4>
+      </div>
+
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you?</p>
+          <p className="sidebar_statNumber">2,543</p>
+        </div>
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className="sidebar_statNumber">2,543</p>
+        </div>
+      </div>
+
+      <div className="sidebar_bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
